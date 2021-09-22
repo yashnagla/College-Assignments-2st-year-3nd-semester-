4437d4b2de7097ad8132807fb4cdd628bf0f6b16
Yash Nagla <84613654+yashnagla@users.noreply.github.com>
2021-09-22 12:11:23 +0530

    Create Sum.cpp

---
+++ Sum.cpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"pls enter first variable";
+    cin >> a;
+    cout<<"pls enter second variable";
+    cin >> b;
+    int sum=a+b;
+    cout<<"sum of the two variable is"<<a<<"and value of b="<<b<<"sum="<<sum;
+    
+    return 0;
+}
